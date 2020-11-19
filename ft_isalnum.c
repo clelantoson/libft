51a6d78448527875c903c2dc450fff9c9704826f
Camille Le Lan-Toson <clelantoson@hotmail.fr>
2020-11-19 13:12:02 +0100

    tests ok until isascii

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,13 +6,15 @@
 /*   By: cle-lan <cle-lan@42.student.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 18:43:10 by cle-lan           #+#    #+#             */
-/*   Updated: 2020/11/18 18:53:47 by cle-lan          ###   ########.fr       */
+/*   Updated: 2020/11/19 12:40:41 by cle-lan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	isdigit(int c)
+#include "libft.h"
+
+int	ft_isalnum(int c)
 {
-	if (c >= '0' && c <= '9')
+	if (ft_isalpha(c) || ft_isdigit(c))
 		return (1);
 	return (0);
 }
