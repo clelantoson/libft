d06763af35f6da04f253ab0714a4c1c7bd736521
Camille Le Lan-Toson <clelantoson@hotmail.fr>
2020-11-19 17:39:24 +0100

    add files tests ok + norminette until tolower

---
+++ libft.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cle-lan <cle-lan@42.student.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/18 15:29:43 by cle-lan           #+#    #+#             */
+/*   Updated: 2020/11/19 17:30:14 by cle-lan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <stdlib.h>
+#include <string.h>
+
+size_t 	ft_strlen(const char *str);
+int		ft_isalpha(int c);
+int 	ft_isdigit(int c);
+int 	ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+
+#endif
