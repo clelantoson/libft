72d375255908a299808bc706523c1d8990ced6ed
Camille Le Lan-Toson <clelantoson@hotmail.fr>
2020-11-20 13:44:31 +0100

    atoi ok on 5 tests but timout on the rest

---
+++ ft_atoi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cle-lan <cle-lan@42.student.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/20 11:59:34 by cle-lan           #+#    #+#             */
+/*   Updated: 2020/11/20 13:22:27 by cle-lan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+int	ft_atoi(const char *str)
+{
+	int i;
+	int sign;
+	int result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (ft_isdigit(str[i]))
+	{
+		result = result * 10 + str[i] - 48;
+	}
+	return (result * sign);
+}
