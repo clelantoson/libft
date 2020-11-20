9be5890394f064926b4bea04e465308a994973aa
Camille Le Lan-Toson <clelantoson@hotmail.fr>
2020-11-20 13:46:13 +0100

    update libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cle-lan <cle-lan@42.student.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 15:29:43 by cle-lan           #+#    #+#             */
-/*   Updated: 2020/11/19 17:30:14 by cle-lan          ###   ########.fr       */
+/*   Updated: 2020/11/20 12:00:52 by cle-lan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,7 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_atoi(const char *str);
 
 #endif
