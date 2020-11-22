270577e770e426c5f89ba99efd0b17aa8ae2bbba
Camille Le Lan-Toson <clelantoson@hotmail.fr>
2020-11-22 13:40:26 +0100

    strlcat fail on two tests

---
+++ ft_strlcat.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cle-lan <cle-lan@42.student.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/20 15:40:49 by cle-lan           #+#    #+#             */
+/*   Updated: 2020/11/20 18:45:13 by cle-lan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t i;
+	size_t j;
+	size_t srclen;
+	size_t dstlen;
+
+	i = 0;
+	j = 0;
+	dstlen = ft_strlen(dst);
+	srclen = ft_strlen(src);
+
+	if (dstsize == 0)
+		return srclen;
+
+	while (dst[i])
+		i++;
+	while (src[j] && j < dstsize -1)
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	if (dstsize < dstlen)
+		return (srclen + dstsize);
+	if (i < dstsize)
+		dst[i + j] = '\0';
+	return (i + srclen);
+}
