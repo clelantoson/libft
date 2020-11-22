042fdc5c4cfe4322cfcd0a9a80f34f0268e1e3dd
Camille Le Lan-Toson <clelantoson@hotmail.fr>
2020-11-22 13:39:32 +0100

    atoi working

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,33 +6,34 @@
 /*   By: cle-lan <cle-lan@42.student.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/20 11:59:34 by cle-lan           #+#    #+#             */
-/*   Updated: 2020/11/20 13:22:27 by cle-lan          ###   ########.fr       */
+/*   Updated: 2020/11/20 23:24:37 by cle-lan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-int	ft_atoi(const char *str)
+int			ft_atoi(const char *str)
 {
-	int i;
-	int sign;
-	int result;
+	int		i;
+	int		nbr;
+	int		sign;
 
 	i = 0;
+	nbr = 0;
 	sign = 1;
-	result = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
 		i++;
-	if (str[i] == '-')
+	if (str[i] == '+' || str[i] == '-')
 	{
-		sign = -1;
+		if (str[i] == '-')
+			sign = -1;
 		i++;
 	}
-	else if (str[i] == '+')
-		i++;
-	while (ft_isdigit(str[i]))
+	while (str[i] && ft_isdigit(str[i]))
 	{
-		result = result * 10 + str[i] - 48;
+		nbr = nbr * 10 + str[i] - '0';
+		i++;
 	}
-	return (result * sign);
+	return (nbr * sign);
 }
+
