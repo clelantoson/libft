242fec1c611790e0d82592427113d33d4cae637a
Camille Le Lan-Toson <clelantoson@hotmail.fr>
2020-11-23 17:34:13 +0100

    working bzero

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cle-lan <cle-lan@42.student.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 15:29:43 by cle-lan           #+#    #+#             */
-/*   Updated: 2020/11/20 17:30:20 by cle-lan          ###   ########.fr       */
+/*   Updated: 2020/11/23 15:05:11 by cle-lan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-size_t 	ft_strlen(const char *str);
-int		ft_isalpha(int c);
-int 	ft_isdigit(int c);
-int 	ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_atoi(const char *str);
-size_t 	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t 			ft_strlen(const char *str);
+int				ft_isalpha(int c);
+int 			ft_isdigit(int c);
+int 			ft_isalnum(int c);
+int				ft_isascii(int c);
+int				ft_isprint(int c);
+int				ft_toupper(int c);
+int				ft_tolower(int c);
+int				ft_strncmp(const char *s1, const char *s2, size_t n);
+int				ft_atoi(const char *str);
+size_t	 		ft_strlcat(char *dst, const char *src, size_t dstsize);
+unsigned int 	ft_strlcpy(char *dest, char *src, unsigned int size);
+void			*ft_memset(void *b, int c, size_t len);
 
 #endif
