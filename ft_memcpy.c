796b97a5194a2fba38b5f893254b1fa08f3cde4c
Camille Le Lan-Toson <clelantoson@hotmail.fr>
2020-11-24 10:07:32 +0100

    norm ok for memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,17 +6,17 @@
 /*   By: cle-lan <cle-lan@42.student.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 17:35:22 by cle-lan           #+#    #+#             */
-/*   Updated: 2020/11/23 21:02:54 by cle-lan          ###   ########.fr       */
+/*   Updated: 2020/11/24 10:07:05 by cle-lan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void			*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t i;
-	char *dstcpy;
-	const char *srccpy;
+	size_t		i;
+	char		*dstcpy;
+	const char	*srccpy;
 
 	dstcpy = (char *)dst;
 	srccpy = (const char *)src;
